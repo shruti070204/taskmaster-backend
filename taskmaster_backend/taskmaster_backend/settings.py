ac3a7ce3f4b0ccb2f82b1c875d23dde30acd0be2
Shruti Mahadik <shrutimahadik2102@gmail.com>
2025-07-15 16:03:52 +0530

    Add Render domain to ALLOWED_HOSTS

--- taskmaster_backend/taskmaster_backend/settings.py
+++ taskmaster_backend/taskmaster_backend/settings.py
@@ -49,9 +49,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware', #allows cross origin requests
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,9 +122,12 @@ USE_TZ = True
 # url prefix Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
