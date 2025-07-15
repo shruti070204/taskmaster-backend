6493b96792a71bdf4671f6fa9b4601f78fd6e563
Shruti Mahadik <shrutimahadik2102@gmail.com>
2025-07-15 17:01:23 +0530

    Fix CORS for frontend domain

--- taskmaster_backend/taskmaster_backend/settings.py
+++ taskmaster_backend/taskmaster_backend/settings.py
@@ -165,8 +165,10 @@ CORS_ALLOW_ALL_ORIGINS=True # allows all domain
 CORS_ALLOWED_ORIGINS = [ #retrict to specific ones
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    'https://taskmaster-frontend-iota.vercel.app',
+    "https://taskmaster-frontend-iota.vercel.app",
 ]
+
+CORS_ALLOW_CREDENTIALS = True
 #gmail  smtp
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
