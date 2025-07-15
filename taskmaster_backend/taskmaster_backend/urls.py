e06a93206a20b67efac13d67f4e6a099cb87bc30
Shruti Mahadik <shrutimahadik2102@gmail.com>
2025-07-15 17:35:56 +0530

    Add JWT /api/token/ endpoint

--- taskmaster_backend/taskmaster_backend/urls.py
+++ taskmaster_backend/taskmaster_backend/urls.py
@@ -2,6 +2,10 @@
 from django.contrib import admin #buitin admin mod acess to admin.site.urls
 from django.urls import path,include # define individual url routes, includes url patterns from other apps
 from django.http import HttpResponse #retun simp res directly from view func
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 def home(request):
     return HttpResponse("""
@@ -24,4 +28,6 @@ urlpatterns = [
     path('',home), #binds root url / to home view
     path('admin/', admin.site.urls), #en jango interface at url /admin/
     path('api/', include('api.urls')), #this connects to app's url
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] 
