921ceb0aaf47ea7a751c07d698091a54bf7b0749
Shruti Mahadik <shrutimahadik2102@gmail.com>
2025-07-15 09:33:38 +0530

    Initial commit

--- taskmaster_backend/taskmaster_backend/settings.py
+++ taskmaster_backend/taskmaster_backend/settings.py
@@ -14,7 +14,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from decouple import config
 import dj_database_url
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
